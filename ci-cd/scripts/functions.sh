424cebe24ad05648db3505248e8c13559bc0f021
Yonathan <ycedillos@applaudostudios.com>
2021-06-16 17:09:27 -0600

    test

---
+++ ci-cd/scripts/functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+install_graphql_scheme_registry () {
+  docker build -f Dockerfile \
+              --no-cache \
+              --build-arg env=production \
+              -t graphql-schema-registry .
+
+  docker tag graphql-schema-registry ycedillos/graphql-schema-registry:1.2.4
+
+  docker push ycedillos/graphql-schema-registry:1.2.4
+
+}
